b48c7364a8daf135fe5bee729080c53f1c79b65c
Tok736 <to_ev@mail.ru>
2024-04-03 13:20:21 +0300

    Uploaded project

---
+++ config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    """Application config."""
+    debug: bool = False
+    database_url: str
+    access_token_expire_minutes: int = 15
+    refresh_token_expire_days: int = 7
+    refresh_token_in_cookie: bool = True
+    refresh_token_in_body: bool 
+    use_cors: bool
+
+    class Config:
+        env_file = ".env"
+
+settings = Settings()
